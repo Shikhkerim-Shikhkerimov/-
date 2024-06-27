1f32edb4cd30f0ae77793926a9b785f054a49ec8
Shikhkerimov Shikhkerim <kerim_shikhkerimov@mail.ru>
2024-06-27 20:57:55 +0400

    added a route section map for the mobile version

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 // слайдер
-document.getElementById('next').onclick = function() {
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
-}
+document.getElementById("next").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").appendChild(lists[0]);
+};
 
-document.getElementById('prev').onclick = function() {
-    let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').prepend(lists[lists.length - 1]);
-}
+document.getElementById("prev").onclick = function () {
+  let lists = document.querySelectorAll(".item");
+  document.getElementById("slide").prepend(lists[lists.length - 1]);
+};
 
 // кнопка секции route-section
+const routeBtn = document.querySelector(".nav-btn_routeBtn");
+const routeClose = document.querySelector(".route-section__route_routeClose");
+
+routeBtn.addEventListener("click", function () {
+  document.querySelector(".route-section__route").style.display = "block";
+  document.querySelector(".route-section__route_routeClose").style.display =
+    "block";
+});
+
+routeClose.addEventListener("click", function () {
+  document.querySelector(".route-section__route").style.display = "none";
+});
