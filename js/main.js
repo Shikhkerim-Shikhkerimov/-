360ba1a8ddaa8e71ea0d2534527c8b23b9c1506d
Shikhkerimov Shikhkerim <kerim_shikhkerimov@mail.ru>
2024-07-02 20:19:24 +0400

    slider for the mobile version of the about us section and the tour program section

--- js/main.js
+++ js/main.js
@@ -20,3 +20,26 @@ routeBtn.addEventListener("click", function () {
 routeClose.addEventListener("click", function () {
   document.querySelector(".routeMobile").style.display = "none";
 });
+
+// слайдер для mobileContent секции about-us
+document.getElementById("aboutUsNext").onclick = function () {
+  let lists = document.querySelectorAll(".aboutUsMobile");
+  document.querySelector(".aboutUsWraper").appendChild(lists[0]);
+};
+
+document.getElementById("aboutUsPrev").onclick = function () {
+  let lists = document.querySelectorAll(".aboutUsMobile");
+  document.querySelector(".aboutUsWraper").prepend(lists[lists.length - 1]);
+};
+
+// слайдер для mobileContent секции tour-program
+document.getElementById("tourProgramNext").onclick = function () {
+  let lists = document.querySelectorAll(".mobileContent");
+  document.querySelector(".tourProgramWraper").appendChild(lists[0]);
+};
+
+document.getElementById("tourProgramPrev").onclick = function () {
+  let lists = document.querySelectorAll(".mobileContent");
+  document.querySelector(".tourProgramWraper").prepend(lists[lists.length - 1]);
+};
+
